64a4d4259325763d28ccdee986d136cd2394b701
Hongkai Liu <hongkailiu@users.noreply.github.com>
2015-08-06 13:20:00 -0400

    start test-app project

---
+++ test-app/src/main/groovy/tk/hongkailiu/test/app/Example.groovy
@@ -0,0 +1,15 @@
+//
+// Generated from archetype; please customize.
+//
+
+package tk.hongkailiu.test.app
+
+/**
+ * Example Groovy class.
+ */
+class Example
+{
+    def show() {
+        println 'Hello World'
+    }
+}
---
+++ test-app/src/main/groovy/tk/hongkailiu/test/app/Helper.java
@@ -0,0 +1,15 @@
+//
+// Generated from archetype; please customize.
+//
+
+package tk.hongkailiu.test.app;
+
+/**
+ * Example Java class.
+ */
+public class Helper
+{
+    public void help(final Example example) {
+        example.show();
+    }
+}
