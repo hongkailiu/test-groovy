43ca3b4f7df376e4cd42a9507e0fd949d00292f8
Hongkai Liu <hongkailiu@users.noreply.github.com>
2016-07-11 14:38:38 -0400

    start tutorial in script

---
+++ src/main/script/hello.groovy
@@ -0,0 +1,21 @@
+/**
+ * Created by ehongka on 7/11/16.
+ */
+// java code
+System.out.println("Hello World");
+
+// groovy code
+println "Hello World"
+
+def x = 42
+println x.getClass()
+x = "Hello World"
+println x.getClass()
+
+
+def y = "World"
+println "Hello, $y"
+
+def firstName = "Douglas"
+def name = "Adams"
+println "Hello, ${firstName[0]}. $name"
