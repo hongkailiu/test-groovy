a6104b953510ccf68a551bdd63a19c2057e865c2
hongkailiu <hongkailiu@users.noreply.github.com>
2016-08-01 22:38:12 -0400

    junit4 integration

--- src/test/groovy/tk/hongkailiu/test/groovy/service/TranslationServiceTest.groovy
+++ src/test/groovy/tk/hongkailiu/test/groovy/service/TranslationServiceTest.groovy
@@ -1,12 +1,20 @@
 package tk.hongkailiu.test.groovy.service
 
+import org.junit.Test
+
 
 /**
  * Created by hongkailiu on 2016-08-01.
  */
-class TranslationServiceTest extends GroovyTestCase {
+class TranslationServiceTest {
+    @Test
     void testConvertWithMapCoercion() {
         def service = [convert: { String key -> 'some text' }] as TranslationService
+        assert 'some text' == service.convert('key.text')
+    }
+    @Test
+    void testConvertWithClosureCoercion() {
+        def service = { String key -> 'some text' } as TranslationService
         assert 'some text' == service.convert('key.text')
     }
 }
